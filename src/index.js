8b07464374ab3c02ba4f53369f6dce6894c26e45
Christina Wu <xchristinawu@gmail.com>
2023-01-03 23:09:31 -0800

    Stop player from moving when wild pokemon encountered

--- src/index.js
+++ src/index.js
@@ -135,6 +135,10 @@ function rectangularCollision( {rectangle1, rectangle2} ) {
         rectangle1.position.y + rectangle1.height >= rectangle2.position.y);
 }
 
+const battle = {
+    initiated: false
+};
+
 function animate() {
     window.requestAnimationFrame(animate);
     background.draw();
@@ -147,6 +151,11 @@ function animate() {
     player.draw();
     foreground.draw();
 
+    let moving = true;
+    player.moving = false;
+
+    if (battle.initiated) return;
+    // activate a battle
     if (keys.w.pressed || keys.a.pressed || keys.s.pressed || keys.d.pressed) {
         for (let i = 0; i < battleZones.length; i++) {
             const battleZone = battleZones[i];
@@ -162,14 +171,13 @@ function animate() {
             overlappingArea > (player.width * player.height) / 2 
             && Math.random() < 0.01
             ) {
-                console.log('battle zone collision');
+                console.log('activate battle');
+                battle.initiated = true;
                 break;
             }
         };
     }
 
-    let moving = true;
-    player.moving = false;
     // if keys pressed, move background image (illusion of movement)
     if (keys.w.pressed && lastKey === 'w') {
         player.moving = true;
